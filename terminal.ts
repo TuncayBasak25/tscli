35fa858142e37a7d20fb16ed92e8387993514e47
Tuncay - Muhammet Ali BASAK - ERKI <tuncay.basak@outlook.com>
2022-04-03 12:42:43 +0200

    Move terminal functionality to Terminal class with auto async await methods.

--- terminal.ts
+++ terminal.ts
@@ -1,19 +1,54 @@
 const { exec } = require("child_process");
 
-export default function terminal(command: string): Promise<void> {
-    let next = () => {};
 
-    exec(command, (error: Error, stdout: string, stderr: string) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
+export default class Terminal {
+
+    private static main: Terminal = new Terminal();
+
+    public static run(...commandList: string[]): void {
+        this.main.run(...commandList);
+    }
+
+    private commandList: string[] = [];
+    private onNewCommand(): void { }
+
+    public constructor() {
+        this.listen();
+    }
+
+    private async listen(): Promise<void> {
+        if (this.commandList.length === 0) {
+            await new Promise<void>(resolve => this.onNewCommand = resolve);
         }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-        }
-        console.log(stdout);
 
-        next();
-    });
+        const command = this.commandList.shift() as string;
 
-    return new Promise(resolve => next = resolve);
+        await this.exec(command);
+
+        this.listen();
+    }
+
+    public run(...commandList: string[]): void {
+        this.commandList.push(...commandList);
+
+        this.onNewCommand();
+    }
+
+    private async exec(command: string): Promise<void> {
+        let next = () => { };
+
+        exec(command, (error: Error, stdout: string, stderr: string) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+            }
+            console.log(stdout);
+
+            next();
+        });
+
+        return new Promise(resolve => next = resolve);
+    }
 }
